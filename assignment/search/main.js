c82ff9f2be26f1a571a22b30e858ecdb7df0cecc
Anil Stha <anilshr25@gmail.com>
2019-12-27 08:48:36 +0545

    update console log

--- assignment/search/main.js
+++ assignment/search/main.js
@@ -2,7 +2,8 @@ var fruits = [{id:"1",name:"Apple",color:"red"},
 				{id:"2",name:"Banana",color:"yellow"}];
 	function searchByName(name) {
 		for (var i = 0; i < fruits.length; i++) {
-			if(fruits[i].name == name)
+			var str = fruits[i].name;
+			if(str.toLowerCase() == name.toLowerCase())
 			{
 				console.log(fruits[i].id);
 				console.log(fruits[i].name);
